f8dc2aadb6e196dbd0631d81b6f274a017664441
Surojiddin <isurojiddin@gmail.com>
2022-06-14 17:41:03 +0500

    add teplate in holiday

--- src/components/pages/Holidays.vue
+++ src/components/pages/Holidays.vue
@@ -4,10 +4,10 @@
     <div class="grid grid-cols-2 gap-1 mb-5">
       <div class="custom-height overflow-y-auto mt-3 px-1">
         <div class="grid grid-cols-2 gap-2">
-          
           <div v-for="(holiday, index) in holidays" :key="index" class="relative h-52 rounded-lg from-blue-100 via-blue-300 to-blue-500 bg-gradient-to-br">
             <div class="actions absolute right-0 top-2 flex justify-end items-center px-1 w-11 cursor-pointer rounded-full">
               <div class="flex justify-center items-center hidden">
+                <i @click="openModal()" class="fa-solid fa-circle-plus text-gray-700 hover:text-green-600 mr-2"></i>
                 <i class="fa-solid fa-feather-pointed text-gray-700 hover:text-blue-600 mr-2"></i>
                 <i @click="deleteHoliday(holiday.id)" class="fa-solid fa-trash-can text-gray-700 hover:text-red-600 mr-2"></i>
               </div>
@@ -20,10 +20,9 @@
                 </div>
               </div>
               <h3 class="text-lg text-center py-1 leading-normal mb-1 font-semibold text-black">{{ holiday.name }}</h3>
-              <p class="text-gray-500 mb-1 py-1 text-center">{{ holiday.day }} - {{ holiday.month}}</p>
+              <p class="text-gray-500 mb-1 py-1 text-center">{{ holiday.day }} - {{ holiday.month }}</p>
             </div>
           </div>
-
         </div>
       </div>
       <div class="p-3">
@@ -69,6 +68,43 @@
         </div>
       </div>
     </div>
+
+    <div id="add-template-modal" tabindex="-1" class="hidden overflow-y-auto w-full overlay overflow-x-hidden fixed top-0 right-0 left-0 z-40 flex items-center justify-center md:inset-0 h-modal md:h-full">
+      <div class="relative p-4 w-full max-w-2xl h-full md:h-auto text-gray-800">
+        <div class="relative bg-white rounded-lg shadow-lg dark:bg-gray-700 z-50">
+          <button type="button" @click="closeModal()" class="absolute top-3 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+          <h3 class="text-2xl font-extrabold py-5 ml-5">Tug'ilgan kun tabrigi</h3>
+
+          <div class="bg-white rounded-lg p-3 px-5 max-content-h">
+            <form>
+              <div class="mb-6">
+                <label for="man-birthday-input" class="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300">Erkaklar uchun</label>
+                <select id="man-birthday-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option>SMS shablonni tanlang</option>
+                  <option value="male">Tug'ilgan kuningiz bilan</option>
+                  <option value="female">Happy birthday</option>
+                </select>
+              </div>
+              <div class="mb-6">
+                <label for="man-birthday-input" class="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300">Ayollar uchun</label>
+                <select id="man-birthday-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option>SMS shablonni tanlang</option>
+                  <option value="male">Tug'ilgan kuningiz bilan</option>
+                  <option value="female">Happy birthday</option>
+                </select>
+              </div>
+              <hr class="border-gray-200 border-dotted bottom-1 mb-6" />
+              <div class="flex justify-end items-center">
+                <button type="button" @click="closeModal()" class="mr-3 text-white bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center" data-modal-toggle="popup-modal">Yopish</button>
+                <button type="submit" class="mx-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Saqlash</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -84,11 +120,19 @@ import $ from 'jquery'
 function openActions(id) {
   let x = $(`#st-${id}`)
   x.toggleClass('fa-ellipsis-vertical').toggleClass('fa-times')
-  x.parent('.actions').toggleClass('w-11').toggleClass('w-28').toggleClass('shadow').toggleClass('right-1')
+  x.parent('.actions').toggleClass('w-11').toggleClass('w-32').toggleClass('shadow').toggleClass('right-1')
   x.parent('.actions').children('div').toggleClass('hidden')
 }
 
 const store = useStore()
+
+const openModal = () => {
+  $('#add-template-modal').removeClass('hidden')
+}
+
+const closeModal = () => {
+  $('#add-template-modal').addClass('hidden')
+}
 
 const templateCategoryId_ = ref("Bo'limni tanlang")
 const name_ = ref('')
@@ -173,5 +217,8 @@ onMounted(() => addHolidayInStore())
 <style scoped>
 .custom-height {
   height: 75vh;
+}
+.overlay {
+  background-color: rgba(0, 0, 0, 0.3);
 }
 </style>
