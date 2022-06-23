c807ffc2cd69d5f29655fb19bdd55e3a470b52cd
Surojiddin <isurojiddin@gmail.com>
2022-06-23 17:47:19 +0500

    setting bug fixed

--- src/components/pages/Settings.vue
+++ src/components/pages/Settings.vue
@@ -9,18 +9,18 @@
           <form @submit.prevent="validateSetting(settings)">
             <div class="mb-1">
               <div class="flex items-center justify-between border-b border-dotted py-3">
-                <div class="text-xl font-medium text-gray-900">Bayramlarda avtomatik tabrik jo'natish</div>
-                <label for="holiday-toggle" class="relative mr-3 inline-flex cursor-pointer items-center">
-                  <input v-model="editSettingParam.autoSendHoliday" :checked="settings.autoSendHoliday" type="checkbox" id="holiday-toggle" class="peer sr-only" />
+                <div class="text-xl font-medium text-gray-900">Tug'ilgan kunlarda avtomatik tabrik jo'natish</div>
+                <label for="birthday-toggle" class="relative mr-3 inline-flex cursor-pointer items-center">
+                  <input v-model="autoSendBirthdayVal" type="checkbox" id="birthday-toggle" class="peer sr-only" />
                   <div class="h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none dark:border-gray-600 dark:bg-gray-700"></div>
                 </label>
               </div>
             </div>
             <div class="mb-1">
               <div class="flex items-center justify-between border-b border-dotted py-3">
-                <div class="text-xl font-medium text-gray-900">Tug'ilgan kunlarda avtomatik tabrik jo'natish</div>
-                <label for="birthday-toggle" class="relative mr-3 inline-flex cursor-pointer items-center">
-                  <input v-model="editSettingParam.autoSendBirthday" :checked="settings.autoSendBirthday" type="checkbox" id="birthday-toggle" class="peer sr-only" />
+                <div class="text-xl font-medium text-gray-900">Bayramlarda avtomatik tabrik jo'natish</div>
+                <label for="holiday-toggle" class="relative mr-3 inline-flex cursor-pointer items-center">
+                  <input v-model="autoSendHolidayVal" :checked="settings.autoSendHoliday" type="checkbox" id="holiday-toggle" class="peer sr-only" />
                   <div class="h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none dark:border-gray-600 dark:bg-gray-700"></div>
                 </label>
               </div>
@@ -46,7 +46,7 @@
 </template>
 
 <script setup>
-import { onMounted, computed, reactive } from 'vue'
+import { onMounted, computed, ref } from 'vue'
 import { useStore } from 'vuex'
 import notify from 'izitoast'
 import 'izitoast/dist/css/iziToast.min.css'
@@ -54,11 +54,15 @@ import settingService from '../../services/setting.service'
 
 const store = useStore()
 
-const editSettingParam = reactive({
+const editSettingParam = {
   autoSendBirthday: false,
   autoSendHoliday: false,
   darkTheme: false,
-})
+}
+
+const autoSendHolidayVal = ref(false)
+const autoSendBirthdayVal = ref(false)
+const darkThemeVal = ref(false)
 
 const addSettingInStore = () => {
   settingService.getSettings().then((data) => store.commit('setSetting', data))
@@ -68,31 +72,18 @@ const settings = computed(() => {
   return store.state.settings
 })
 
-const saveSettings = (setting) => {
-  editSettingParam.autoSendBirthday = setting.autoSendBirthday
-  editSettingParam.autoSendHoliday = setting.autoSendHoliday
-  editSettingParam.darkTheme = setting.darkTheme
-}
-saveSettings(store.state.settings)
+autoSendBirthdayVal.value = store.state.settings.autoSendBirthday
+autoSendHolidayVal.value = store.state.settings.autoSendHoliday
+darkThemeVal.value = store.state.settings.darkTheme
 
-const validateSetting = (setting) => {
-  if (editSettingParam.autoSendBirthday === undefined && editSettingParam.autoSendHoliday === undefined && editSettingParam.darkTheme === undefined) {
-    notify.warning({
-      message: 'Tizim sozlamalarini o`zgartirmadingiz!',
-      position: 'bottomLeft',
-    })
-  } else if (editSettingParam.autoSendBirthday === true || editSettingParam.autoSendBirthday === false) {
-    editSettingParam.autoSendHoliday = setting.autoSendHoliday
-    editSettingParam.darkTheme = setting.darkTheme
-    updateSettings()
-  } else if (editSettingParam.autoSendHoliday === true || editSettingParam.autoSendHoliday === false) {
-    editSettingParam.autoSendBirthday = setting.autoSendBirthday
-    editSettingParam.darkTheme = setting.darkTheme
-    updateSettings()
-  }
+const validateSetting = (settings) => {
+  editSettingParam.autoSendBirthday = autoSendBirthdayVal.value
+  editSettingParam.autoSendHoliday = autoSendHolidayVal.value
+  editSettingParam.darkTheme = darkThemeVal.value
+  updateSettings(editSettingParam)
 }
 
-const updateSettings = () => {
+const updateSettings = (editSettingParam) => {
   store.dispatch('settingsModule/update', editSettingParam).then(
     () => {
       notify.success({
