8001a40f4a4d01a705504b55632d3266de01918f
OG'ABEK <shomoyxhacker007@mail.ru>
2022-06-24 01:06:55 -0500

    login page validaion changed

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="bg-emerald-100 h-screen w-screen">
-    <div class="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0">
-      <div class="flex rounded-3xl shadow-lg w-full h-3/4 sm:w-4/5 lg:w-2/3 bg-white sm:mx-0">
-        <div class="flex flex-col w-full md:w-1/2 p-4">
+  <div class="h-screen w-screen bg-emerald-100">
+    <div class="flex h-full flex-1 flex-col items-center justify-center px-4 sm:px-0">
+      <div class="flex h-3/4 w-full rounded-3xl bg-white shadow-lg sm:mx-0 sm:w-4/5 lg:w-2/3">
+        <div class="flex w-full flex-col p-4 md:w-1/2">
           <div class="flex items-center">
-            <img src="../assets/images/logo.png" class="shrink-0 w-8 ml-2" alt="#" />
-            <div class="grow font-semibold text-lg text-gray-900 ml-2">IT-Forelead</div>
+            <img src="../assets/images/logo.png" class="ml-2 w-8 shrink-0" alt="#" />
+            <div class="ml-2 grow text-lg font-semibold text-gray-900">IT-Forelead</div>
           </div>
-          <div class="flex flex-col flex-1 justify-center mb-5">
-            <h3 class="text-4xl text-center font-semibold">SMS-PLATFORM</h3>
-            <p class="text-center text-sm px-10 text-gray-500 my-5">Mijozlaringizni tug'ilgan kunlarida va bayramlarda hech qanday vaqt sarflamasdan avtamatik tarizda tabriklang!</p>
-            <div class="w-full mt-8">
-              <Form @submit="onSubmit" :validation-schema="schema" class="form-horizontal w-3/4 mx-auto" method="POST" action="#">
-                <div class="flex flex-col mt-4">
-                  <Field v-model="email" name="email" type="email" class="bg-gray-100 text-gray-500 p-4 text-md w-full rounded-xl focus:outline-none focus:bg-gray-200 outline-none border-0" placeholder="Emailni kiriting..." />
-                  <ErrorMessage name="email" class="error-feedback text-rose-600 font-medium" />
+          <div class="mb-5 flex flex-1 flex-col justify-center">
+            <h3 class="text-center text-4xl font-semibold">SMS-PLATFORM</h3>
+            <p class="my-5 px-10 text-center text-sm text-gray-500">Mijozlaringizni tug'ilgan kunlarida va bayramlarda hech qanday vaqt sarflamasdan avtamatik tarizda tabriklang!</p>
+            <div class="mt-8 w-full">
+              <Form @submit="onSubmit" :validation-schema="schema" class="form-horizontal mx-auto w-3/4" method="POST" action="#">
+                <div class="mt-4 flex flex-col">
+                  <Field v-model="email" name="email" type="email" class="text-md w-full rounded-xl border-0 bg-gray-100 p-4 text-gray-500 outline-none focus:bg-gray-200 focus:outline-none" placeholder="Emailni kiriting..." />
+                  <ErrorMessage name="email" class="error-feedback font-medium text-rose-600" />
                 </div>
-                <div class="flex flex-col mt-4">
-                  <Field v-model="password" name="password" type="password" class="bg-gray-100 text-gray-500 p-4 text-md w-full rounded-xl focus:outline-none focus:bg-gray-200 outline-none border-0" placeholder="Parolni kiriting..." />
-                  <ErrorMessage name="password" class="error-feedback text-rose-600 font-medium" />
+                <div class="mt-4 flex flex-col">
+                  <Field v-model="password" name="password" type="password" class="text-md w-full rounded-xl border-0 bg-gray-100 p-4 text-gray-500 outline-none focus:bg-gray-200 focus:outline-none" placeholder="Parolni kiriting..." />
+                  <ErrorMessage name="password" class="error-feedback font-medium text-rose-600" />
                 </div>
-                <div class="flex flex-col mt-8">
-                  <button type="submit" class="bg-gray-900 hover:bg-gray-800 text-white py-5 text-md w-full rounded-xl">Tizimga kirish</button>
+                <div class="mt-8 flex flex-col">
+                  <button type="submit" class="text-md w-full rounded-xl bg-gray-900 py-5 text-white hover:bg-gray-800">Tizimga kirish</button>
                 </div>
               </Form>
-              <div class="text-center mt-8">
-                <a class="no-underline hover:underline cursor-pointer text-rose-600 text-sm">Parolni unitdingizmi?</a>
+              <div class="mt-8 text-center">
+                <a class="cursor-pointer text-sm text-rose-600 no-underline hover:underline">Parolni unitdingizmi?</a>
               </div>
             </div>
           </div>
-          <div class="text-xs text-gray-400 text-center dark:text-gray-400">All rights reserved. &copy; <a href="https://it-forelead.uz" class="hover:underline">IT-Forelead</a> 2022</div>
+          <div class="text-center text-xs text-gray-400 dark:text-gray-400">All rights reserved. &copy; <a href="https://it-forelead.uz" class="hover:underline">IT-Forelead</a> 2022</div>
         </div>
-        <div class="bg-image md:block md:w-2/3 rounded-r-3xl">
-          <div class="flex justify-center items-center h-full">
-            <div class="w-full text-white backdrop-blur-sm backdrop-contrast-50 rounded-xl px-10 py-16 mx-32">
-              <h3 class="text-3xl font-semibold mb-5">Lorem ipsum, dolor sit amet consectetur elit.</h3>
+        <div class="bg-image rounded-r-3xl md:block md:w-2/3">
+          <div class="flex h-full items-center justify-center">
+            <div class="mx-32 w-full rounded-xl px-10 py-16 text-white backdrop-blur-sm backdrop-contrast-50">
+              <h3 class="mb-5 text-3xl font-semibold">Lorem ipsum, dolor sit amet consectetur elit.</h3>
               <p class="text-md">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit optio fuga, itaque deleniti facilis accusamus aliquam sint ad laborum nam?</p>
             </div>
           </div>
@@ -70,24 +70,33 @@ function checkLogin(data) {
   store.commit('setLogin', data)
 }
 
+const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i //email regex
+
 const onSubmit = (user) => {
-  store.dispatch('auth/login', user).then(
-    () => {
-      router.push('/')
-      checkLogin(true)
-      setInterval(() => {
-        authService.logout()
-        router.push('/login')
-        checkLogin(false)
-      }, 500 * 60 * 60)
-    },
-    (error) => {
-      iziToast.error({
-        message: (error.response && error.response.data && error.response.data.message) || error.message || error.toString(),
-        position: 'topRight',
-      })
-    }
-  )
+  if (!regex.test(user.email)) {
+    iziToast.warning({
+      message: "Iltimos, to'g'ri email kiriting! Masalan namuna@misol.uz",
+      position: 'topRight',
+    })
+  } else {
+    store.dispatch('auth/login', user).then(
+      () => {
+        router.push('/')
+        checkLogin(true)
+        setInterval(() => {
+          authService.logout()
+          router.push('/login')
+          checkLogin(false)
+        }, 500 * 60 * 60)
+      },
+      (error) => {
+        iziToast.error({
+          message: "Email toki parol noto'g'ri!",
+          position: 'topRight',
+        })
+      }
+    )
+  }
 }
 </script>
 
